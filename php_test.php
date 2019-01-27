12e9043acab0dfd71d7866f13629ac3cde7fdd06
mypark7867 <mypark7867@gmail.com>
2019-01-28 00:39:22 +0900

    복습을 위해 web1.html파일을 새로 작성.
    php_test.php파일의 home링크를 http://localhost/index.html로 수정

--- php_test.php
+++ php_test.php
@@ -5,24 +5,25 @@
     <title>PHP 예제</title>
   </head>
 <style>
-  h1 {
+  .h1_php {
     text-align: center;
     background-color: #79BEA8; //w3schools.com/colors/colors_monochromatic.asp
     color: #093426; //위 사이트의 컬러 조합 참고함.
     font-size: 33px;
   }
-  h2 {
+  .h2_php {
     text-align: center;
     background-color: #E7F3EF; //w3schools.com/colors/colors_monochromatic.asp
     color: #23483C; //위 사이트의 컬러 조합 참고함.
     font-size: 20px;
   }
 </style>
-  <body>
-<h2><a href="./index.html">HOME</a></h2>
-<h1><abbr title="PHP는 PHP Hyper Text로 재귀적 약어이다.">PHP</abbr> 연습하기</h1>
 
-<h2>여기 부터  php예제가 시작됩니다.</h2>
+  <body>
+
+<h2 class="h2_php"><a href="http://localhost/index.html">HOME</a></h2>
+<h1 class="h1_php"><abbr title="PHP는 PHP Hyper Text로 재귀적 약어이다.">PHP</abbr> 연습하기</h1>
+<h2 class="h2_php">여기 부터  php예제가 시작됩니다.</h2>
 <?php
 $date=date_create("2013-03-15");
 echo date_format($date,"Y/m/d H:i:s");
@@ -41,6 +42,7 @@ echo date_format($date,"Y/m/d H:i:s");
  ?>
 
 <!-- <?php phpinfo(); ?> -->
-<h2>여기까지가 php예제입니다.</h2>
+<h2 class="h2_php">여기까지가 php예제입니다.</h2>
+
   </body>
 </html>
